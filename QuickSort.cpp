bfba5abeebfc3a85438c7bb027c628277e8dbc98
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-03-28 15:03:04 +0700

    menambahkan prosedur swap untuk menukar elemen pada index x dengan elemen pada index y

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -26,6 +26,13 @@ void input() {
     }
 }
 
+void swap(int x, int y) {
+    int temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+    mov_count++;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
