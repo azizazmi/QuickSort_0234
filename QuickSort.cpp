3510cb6187cea90ed569f0d5192c94c39afd2a21
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-03-28 15:23:26 +0700

    menambahkan step 9 pada prosedur quick sort

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -55,7 +55,11 @@ void q_short(int low, int high) {
             j--;   //step 8
             cmp_count++;
         }
-}
+        cmp_count++;
+
+        if (i < j) {   //step 9
+            swap(i, j);
+        }
 
 int main()
 {
